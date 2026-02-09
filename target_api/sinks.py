305292abe3219d2ff34a069e67e05fca0c24bb82
nagumalli <vinay@different.ai>
2026-02-09 13:41:02 +0530

    decreasing batch size to 10

--- target_api/sinks.py
+++ target_api/sinks.py
@@ -77,10 +77,10 @@ class BatchSink(ApiSink, HotglueBatchSink):
     @property
     def max_size(self):
         if self.config.get("process_as_batch"):
-            batch_size = self.config.get("batch_size", 20)
+            batch_size = self.config.get("batch_size", 10)
             if batch_size:
                 return int(batch_size)
-        return 20
+        return 10
 
     def process_batch_record(self, record: dict, index: int) -> dict:
         if self.config.get("add_stream_key"):
